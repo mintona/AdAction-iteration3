7a371567c1678482ac070260147265286169f2bc
Alison Vermeil <alison.minton@gmail.com>
2020-04-18 12:38:01 -0600

    Add run method to FizzBuzz, test passing

--- src/main/java/fizzBuzz/FizzBuzz.java
+++ src/main/java/fizzBuzz/FizzBuzz.java
@@ -1,5 +1,26 @@
 package fizzBuzz;
 
-public class FizzBuzz {
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
+public class FizzBuzz {
+    public static String run(int start, int end) {
+        int[] numbers = IntStream.rangeClosed(start, end).toArray();
+
+        ArrayList<String> results = new ArrayList<String>();
+
+        for (int number : numbers) {
+            if (number % 15 == 0) {
+                results.add("fizzbuzz");
+            } else if (number % 3 == 0) {
+                results.add("fizz");
+            } else if (number % 5 == 0) {
+                results.add("buzz");
+            } else {
+                results.add(Integer.toString(number));
+            }
+        }
+
+        return String.join(" ", results);
+    }
 }
