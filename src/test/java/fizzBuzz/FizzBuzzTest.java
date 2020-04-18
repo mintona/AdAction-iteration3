8b42f6157cf9b01c1ee708adf2805308e0936219
Alison Vermeil <alison.minton@gmail.com>
2020-04-18 12:30:37 -0600

    Add test for .run method

--- src/test/java/fizzBuzz/FizzBuzzTest.java
+++ src/test/java/fizzBuzz/FizzBuzzTest.java
@@ -3,7 +3,15 @@ package fizzBuzz;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.containsString;
 
 public class FizzBuzzTest {
     private FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    public void whenGivenNumbers1through20_theOutputIsCorrect() {
+        String expected = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz";
+
+        assertThat(fizzBuzz.run(1, 20), containsString(expected));
+    }
 }
