5ea72bd8951de456d69223438a7aa13c02510caa
Alison Vermeil <alison.minton@gmail.com>
2020-04-18 13:13:56 -0600

    Add test for generateReport method

--- src/test/java/fizzBuzz/FizzBuzzTest.java
+++ src/test/java/fizzBuzz/FizzBuzzTest.java
@@ -2,7 +2,11 @@ package fizzBuzz;
 
 import static org.junit.Assert.*;
 
+import jdk.jfr.StackTrace;
 import org.junit.Test;
+
+import java.util.Map;
+
 import static org.hamcrest.CoreMatchers.containsString;
 
 public class FizzBuzzTest {
@@ -13,5 +17,17 @@ public class FizzBuzzTest {
         String expected = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz";
 
         assertThat(fizzBuzz.run(1, 20), containsString(expected));
+    }
+
+    @Test
+    public void whenGivenNumbers1through20_aReportCanBeGenerated() {
+        Map<String, Integer> expected = new HashMap<String, Integer>();
+        expected.put("lucky", 2);
+        expected.put("integer", 10);
+        expected.put("fizz", 4);
+        expected.put("fizzbuzz", 1);
+        expected.put("buzz", 3);
+
+        assertEquals(expected, fizzBuzz.generateReport(1,20));
     }
 }
