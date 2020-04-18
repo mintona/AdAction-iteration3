e6b8846685fbfc5d7291c537fa21186515c06c78
Alison Vermeil <alison.minton@gmail.com>
2020-04-18 12:55:21 -0600

    Refactor run test to assert values containing 3 output as lucky

--- src/test/java/fizzBuzz/FizzBuzzTest.java
+++ src/test/java/fizzBuzz/FizzBuzzTest.java
@@ -9,8 +9,8 @@ public class FizzBuzzTest {
     private FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
-    public void whenGivenNumbers1through20_theOutputIsCorrect() {
-        String expected = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz";
+    public void whenGivenNumbers1through20_theOutputWithLuckyIsCorrect() {
+        String expected = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz";
 
         assertThat(fizzBuzz.run(1, 20), containsString(expected));
     }
